102338a5237808bf4bd4d761f00559a616869cf1
dhrubo jouti das troyee  (Dhruv/Troy) <150995673+dhrubojoutidas@users.noreply.github.com>
2026-01-30 22:31:27 +0800

    Add files via upload

---
+++ Embedded_Systems_Engineering/Push_Button_Example.ino
@@ -0,0 +1,34 @@
+#define LEDA LED1
+#define LEDB LED2
+
+volatile int flag = LOW;
+int count = 0;
+
+void setup()
+{
+   pinMode(LEDA, OUTPUT);
+   pinMode(LEDB, OUTPUT);     
+   pinMode(PUSH2, INPUT_PULLUP);
+  attachInterrupt(PUSH2, blink, FALLING); // Interrupt is fired whenever button is pressed
+}
+
+void loop()
+{
+if(flag) {
+ digitalWrite(LEDA, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDA, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);               // wait for a secon
+  digitalWrite(LEDB, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDB, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);     
+ flag = LOW;
+  }
+}
+
+void blink()
+{
+  flag = HIGH;
+}
+
---
+++ Embedded_Systems_Engineering/Round_Robin_Example.ino
@@ -0,0 +1,38 @@
+#define LEDA LED1
+#define LEDB LED2
+#define LEDC LED3
+#define LEDD LED4
+
+//see pins_energia.h for more LED definitions
+//#define LED GREEN_LED
+  
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(LEDA, OUTPUT);
+  pinMode(LEDB, OUTPUT);     
+  pinMode(LEDC, OUTPUT);     
+  pinMode(LEDD, OUTPUT);     
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(LEDA, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDA, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);               // wait for a second
+  digitalWrite(LEDB, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDB, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);     
+  digitalWrite(LEDC, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDC, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);     
+  digitalWrite(LEDD, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(LEDD, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);     
+}
+
